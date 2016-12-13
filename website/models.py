af3f8dcb68475fd8619899dc8580b24bd8b9331c
sagarmanghnani <sagarmanghnani@gmail.com>
2016-12-13 17:23:37 +0530

    created table for login

--- website/models.py
+++ website/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.db import models
 from django.db import models
 
 # Create your models here.
+
+class SignNer(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
